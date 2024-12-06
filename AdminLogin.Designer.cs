5d768789c558a3c71c44725c41d6f27e6268faba
mahbubalam121 <67554980+mahbubalam121@users.noreply.github.com>
2024-12-07 00:35:15 +0600

    Add files via upload

---
+++ AdminLogin.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace Blood
+{
+    partial class AdminLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges1 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges2 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            guna2Button1 = new Guna.UI2.WinForms.Guna2Button();
+            textBox2 = new TextBox();
+            label3 = new Label();
+            label4 = new Label();
+            label1 = new Label();
+            label5 = new Label();
+            SuspendLayout();
+            // 
+            // guna2Button1
+            // 
+            guna2Button1.BorderRadius = 20;
+            guna2Button1.CustomizableEdges = customizableEdges1;
+            guna2Button1.DisabledState.BorderColor = Color.DarkGray;
+            guna2Button1.DisabledState.CustomBorderColor = Color.DarkGray;
+            guna2Button1.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
+            guna2Button1.DisabledState.ForeColor = Color.FromArgb(141, 141, 141);
+            guna2Button1.FillColor = Color.Brown;
+            guna2Button1.Font = new Font("Segoe UI", 12F, FontStyle.Bold);
+            guna2Button1.ForeColor = Color.White;
+            guna2Button1.Location = new Point(311, 240);
+            guna2Button1.Name = "guna2Button1";
+            guna2Button1.ShadowDecoration.CustomizableEdges = customizableEdges2;
+            guna2Button1.Size = new Size(180, 45);
+            guna2Button1.TabIndex = 56;
+            guna2Button1.Text = "Login";
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(341, 150);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(200, 23);
+            textBox2.TabIndex = 55;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 15.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label3.Location = new Point(221, 145);
+            label3.Name = "label3";
+            label3.Size = new Size(105, 30);
+            label3.TabIndex = 54;
+            label3.Text = "Password";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold | FontStyle.Underline, GraphicsUnit.Point, 0);
+            label4.Location = new Point(363, 316);
+            label4.Name = "label4";
+            label4.Size = new Size(70, 25);
+            label4.TabIndex = 53;
+            label4.Text = "Cancel";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Rockwell", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.ForeColor = Color.Brown;
+            label1.Location = new Point(190, 23);
+            label1.Name = "label1";
+            label1.Size = new Size(404, 29);
+            label1.TabIndex = 52;
+            label1.Text = "Blood Bank Management System";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label5.Location = new Point(752, 31);
+            label5.Name = "label5";
+            label5.Size = new Size(20, 21);
+            label5.TabIndex = 57;
+            label5.Text = "X";
+            // 
+            // AdminLogin
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(784, 367);
+            Controls.Add(label5);
+            Controls.Add(guna2Button1);
+            Controls.Add(textBox2);
+            Controls.Add(label3);
+            Controls.Add(label4);
+            Controls.Add(label1);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "AdminLogin";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "AdminLogin";
+            Load += AdminLogin_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Guna.UI2.WinForms.Guna2Button guna2Button1;
+        private TextBox textBox2;
+        private Label label3;
+        private Label label4;
+        private Label label1;
+        private Label label5;
+    }
+}
